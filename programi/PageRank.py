1cc4f8da13f077ed2ddd22685441f85a7e45ad01
Tomislav Novak <tomislav.novak1@student.math.hr>
2024-11-30 23:45:28 +0100

    update code

--- programi/PageRank.py
+++ programi/PageRank.py
@@ -1,8 +1,8 @@
 import scipy.io
 import numpy as np
-from scipy.sparse import coo_matrix, csc_matrix
+from scipy.sparse import coo_matrix
 
-data = scipy.io.loadmat('PageRankTestData.mat')
+data = scipy.io.loadmat(r'..\podaci\PageRankTestData.mat')
 # print(data)
 
 G = data['G']   # 'G' graph matrix representation
@@ -14,23 +14,25 @@ rows = []
 cols = []
 values = []
 for i, j in zip(*G.nonzero()):
-    if n_js[0, j] != 0:
-        rows.append(i)
-        cols.append(j)
-        values.append(1 / n_js[0, j])
-A = coo_matrix((values, (rows, cols)), shape=G.shape).tocsc()   # matrix A
-# print(A.toarray())
+    rows.append(i)
+    cols.append(j)
+    values.append(1 / n_js[0, j])
+A = coo_matrix((values, (rows, cols)), shape=G.shape).tocsc()
 
-alpha = 0.9                 # alpha
-S = np.full((n, n), 1/n)    
+alpha = 0.9                 # alpha 
 x = np.ones(n) / n          # stohastic vector x_0
 
 k = 100
 for i in range(k):          # iterations Mx
-    x = (1 - alpha) * A.dot(x) + alpha * S.dot(x)
+    x = (1 - alpha) * A.dot(x) + np.full(n, alpha * np.sum(x) / n)
 
-x = x / np.linalg.norm(x)   # normalize x (although not necessary for the result)
-pairs = list(zip(x, U))     # pair gotten values with web page representations
+x = x / np.linalg.norm(x)
+pairs = list(zip(x, U))                                                 # pair gotten values with web page representations
 sorted_pairs = sorted(pairs, key=lambda pair: pair[0], reverse=True)    # sort descending by the x_i value
+
+# test print first few
+p = 10
 for value, label in sorted_pairs:
     print(value, label)
+    p -= 1
+    if not p: break
